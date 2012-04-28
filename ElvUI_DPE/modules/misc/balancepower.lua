3e5f6070f76c140894534c24a66cabceb54d801d
Darthpred <darthpred@gmail.com>
2012-04-28 14:47:59 +0400

    Rewrote balance power value frame to be profile based and no global based. Also will need to replace old M functions with DPE

--- ElvUI_DPE/modules/misc/balancepower.lua
+++ ElvUI_DPE/modules/misc/balancepower.lua
@@ -1,18 +1,22 @@
 local E, L, P, G = unpack(ElvUI); --Engine
 local M = E:GetModule('Misc');
+local DPE = E:GetModule('DPE');
 
 --Defaults
 P['general'] = {
-	['bpenable'] = 0,
+	['bpenable'] = false,
 };
 
+local bpower = CreateFrame('Frame', "BPower", E.UIParent)
+local bpower_text = bpower:CreateFontString(nil, 'OVERLAY')
+
 function M:CreateBalancePower()
-	local bpower = CreateFrame('Frame', "BPower", E.UIParent)
+	
 	bpower:Size(30, 10)
 	bpower:Point("BOTTOMLEFT", ElvUF_Player, "TOPLEFT", 2, 3)
 	bpower:CreateBackdrop('Default')
 	
-	local bpower_text = bpower:CreateFontString(nil, 'OVERLAY')
+	
 	bpower_text:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
 	bpower_text:SetPoint("CENTER", bpower, "CENTER")
 	
@@ -22,8 +26,20 @@ function M:CreateBalancePower()
 	end)
 	
 	E:CreateMover(bpower, "BalancePowerMover", "BP")
+	
+	bpower:Hide()
+	
+	DPE:BPUpdate()
 end
 
-if E.myclass == "DRUID" and E.db.general.bpenable then
+function DPE:BPUpdate()
+	if E.db.general.bpenable then
+		bpower:Show()
+	else
+		bpower:Hide()
+	end
+end
+
+if E.myclass == "DRUID" then
 	M:CreateBalancePower()
 end
