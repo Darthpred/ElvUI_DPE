71498972652e320b6c867e9dcbd61e54df247e0e
Darthpred <darthpred@gmail.com>
2012-04-28 14:50:50 +0400

    Added defaults for chat data panels. Their absence caused unapropriate options' loading Also some testing commenting and adding

--- ElvUI_DPE/config/profile.lua
+++ ElvUI_DPE/config/profile.lua
@@ -1,13 +1,27 @@
 ﻿local E, L, P, G = unpack(ElvUI); --Inport: Engine, Locales, ProfileDB, GlobalDB
 
 --Core
-P['general'] = {
+--[[P['general'] = {
 	['pvpautorelease'] = true,
-};
+};]]
+
+P['dpe'] = {
+	['xprepinfo'] = true,
+}
 
 --Datatexts
 P['datatexts'] = {
 	['panels'] = {
+		['LeftChatDataPanel'] = {
+			['left'] = 'Armor',
+			['middle'] = 'Durability',
+			['right'] = 'Avoidance',
+		},
+		['RightChatDataPanel'] = {
+			['left'] = 'System',
+			['middle'] = 'Time',	
+			['right'] = 'Gold',
+		},
 		['Bottom_Right'] = {
 		['left'] = 'Spell/Heal Power',
 		['middle'] = 'Haste',
