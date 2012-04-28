444413e78d3235fda0161e74f7a949afaa2fcc42
Darthpred <darthpred@gmail.com>
2012-04-28 14:39:32 +0400

    Added a new module to control all included miniedits. May be useful if i decide to hook something to update function of ElvUI. Also is used for test my mad ideas.

---
+++ ElvUI_DPE/ElvUI_DPE.lua
@@ -0,0 +1,18 @@
+﻿-------------------------------------------------
+--Here be Credits
+-------------------------------------------------
+local E, L, P, G = unpack(ElvUI); --Engine, Locales, Profile, Global
+local DPE = E:NewModule('DPE', 'AceHook-3.0', 'AceEvent-3.0');
+local M = E:GetModule('Misc');
+
+E.UpdateAllDPE = E.UpdateAll
+function E:UpdateAll()
+    E.UpdateAllDPE(self)
+end
+
+function DPE:Initialize()
+	--For testing purposes. will be deleted
+	print("DPE init")
+end
+
+E:RegisterModule(DPE:GetName())
