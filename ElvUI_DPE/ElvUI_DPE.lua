4f7dd6e2356b75bf7f73a0cf7bf35a62c3d0ef3d
Darthpred <darthpred@gmail.com>
2012-04-28 23:51:42 +0400

    Cleared unused modules from main lua and added force setting auras per row to 19

--- ElvUI_DPE/ElvUI_DPE.lua
+++ ElvUI_DPE/ElvUI_DPE.lua
@@ -3,17 +3,18 @@
 -------------------------------------------------
 local E, L, P, G = unpack(ElvUI); --Engine, Locales, Profile, Global
 local DPE = E:NewModule('DPE', 'AceHook-3.0', 'AceEvent-3.0');
-local M = E:GetModule('Misc');
-local CH = E:GetModule('Chat')
 
 function DPE:Initialize()
 	--For testing purposes.
+	E.db.auras.perRow = 19
 	print("DPE initialized")
 end
 
 E.UpdateAllDPE = E.UpdateAll
 function E:UpdateAll()
     E.UpdateAllDPE(self)
+	
+	E.db.auras.perRow = 19
 end
 
 E:RegisterModule(DPE:GetName())
