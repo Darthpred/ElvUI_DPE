32d156718e6e598ac419c3cd8d2355de7765623c
Darthpred <darthpred@gmail.com>
2012-04-29 13:44:08 +0400

    Returned Update_elements.lua and put updating pvp text to it. For some reason this function refused to work in player.lua Deleted commented testing code form player.lua

---
+++ ElvUI_DPE/modules/unitframes/update_elements.lua
@@ -0,0 +1,34 @@
+local E, L, P, G = unpack(ElvUI); --Inport: Engine, Locales, ProfileDB, GlobalDB
+local UF = E:GetModule('UnitFrames');
+local LSM = LibStub("LibSharedMedia-3.0");
+
+function UF:UpdatePvPText(frame)
+	local unit = frame.unit
+	local PvPText = frame.PvPText
+	local LowManaText = frame.Power.LowManaText
+	
+	PvPText:Show()
+	if LowManaText and LowManaText:IsShown() then LowManaText:Hide() end
+		
+	local time = GetPVPTimer()
+	local min = format("%01.f", floor((time / 1000) / 60))
+	local sec = format("%02.f", floor((time / 1000) - min * 60)) 
+		
+	if(UnitIsPVPFreeForAll(unit)) then
+		if time ~= 301000 and time ~= -1 then
+			PvPText:SetText(PVP.." ".."("..min..":"..sec..")")
+		else
+			PvPText:SetText(PVP)
+		end
+	elseif UnitIsPVP(unit) then
+		if time ~= 301000 and time ~= -1 then
+			PvPText:SetText(PVP.." ".."("..min..":"..sec..")")
+		else
+			PvPText:SetText(PVP)
+		end
+	else
+		PvPText:SetText("")
+	end
+	
+	PvPText:Point("BOTTOMLEFT", frame.Health, "BOTTOMLEFT", 2, 2)
+end
