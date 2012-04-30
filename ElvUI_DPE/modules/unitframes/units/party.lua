594a8683c3143a3a7086dbcafa03edd2ee3d0fa7
Darthpred <darthpred@gmail.com>
2012-04-30 16:17:36 +0400

    Added raid res icon resizing. Didn't see it (not much resing on BGs) but I'm pretty sure it's good.
    Also after couple of testing BGs I've put in checks for normal/raid group so the functions don't try to find unexisting frames.

--- ElvUI_DPE/modules/unitframes/units/party.lua
+++ ElvUI_DPE/modules/unitframes/units/party.lua
@@ -6,7 +6,8 @@ local abs = math.abs
 local _, ns = ...
 local ElvUF = ns.oUF
 
-function UF:Resize_ResIndicator()
+function UF:Resize_PartyResIndicator()
+	if GetNumRaidMembers() > 5 then return end
 	local Pmembers = GetNumPartyMembers() + 1
 	for i = 1, Pmembers do
 		_G["ElvUF_PartyUnitButton"..i].ResurrectIcon:Size(16, 20)
@@ -14,4 +15,4 @@ function UF:Resize_ResIndicator()
 end
 
 UF:Update_AllFrames()
-UF:RegisterEvent("PARTY_MEMBERS_CHANGED", "Resize_ResIndicator")
+UF:RegisterEvent("PARTY_MEMBERS_CHANGED", "Resize_PartyResIndicator")
