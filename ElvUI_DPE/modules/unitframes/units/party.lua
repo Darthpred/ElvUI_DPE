54f1e5a0ae0f6cb3d4780496bdcba70592d15787
Darthpred <darthpred@gmail.com>
2012-04-30 14:37:42 +0400

    Resized Resurrect icon for party frames. I'm not sure if I should reposition it too.
    Raid frames ico is next, but i'll need someone to create a raid.

---
+++ ElvUI_DPE/modules/unitframes/units/party.lua
@@ -0,0 +1,25 @@
+local E, L, P, G = unpack(ElvUI); --Inport: Engine, Locales, ProfileDB, GlobalDB
+local UF = E:GetModule('UnitFrames');
+local LSM = LibStub("LibSharedMedia-3.0");
+
+local abs = math.abs
+local _, ns = ...
+local ElvUF = ns.oUF
+
+function UF:Resize_ResIndicator()
+	local Pmembers = GetNumPartyMembers() + 1
+	for i = 1, Pmembers do
+		_G["ElvUF_PartyUnitButton"..i].ResurrectIcon:Size(16, 20)
+	end
+end
+
+UF.UpdateAllFramesDPE = UF.UpdateAllFrames
+function UF:UpdateAllFrames()
+    UF:UpdateAllFramesDPE(self)
+	
+	UF:Resize_ResIndicator()
+	print("Imma updated")
+end
+
+UF:Update_AllFrames()
+UF:RegisterEvent("PARTY_MEMBERS_CHANGED", "Resize_ResIndicator")
