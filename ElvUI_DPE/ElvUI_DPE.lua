040a85176b341384390b7360426172a7c25a271b
Darthpred <darthpred@gmail.com>
2012-05-02 19:15:46 +0400

    Added datapanels to updating modules during profile change

--- ElvUI_DPE/ElvUI_DPE.lua
+++ ElvUI_DPE/ElvUI_DPE.lua
@@ -6,12 +6,14 @@ local DPE = E:NewModule('DPE', 'AceHook-3.0', 'AceEvent-3.0');
 
 function DPE:Initialize()
 	E.db.auras.perRow = 19 --Forcing more auras to show
+	
 	print("DPE initialized")
 end
 
 E.UpdateAllDPE = E.UpdateAll
 function E:UpdateAll()
     E.UpdateAllDPE(self)
+	E:GetModule('DTPanels'):Update()
 	
 	E.db.auras.perRow = 19 --Forcing more auras to show after profile change as long as it's profile based setting
 end
