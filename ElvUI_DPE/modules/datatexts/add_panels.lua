42acaa8e4f2d670f5b0a6153684f7ca29c0c1b51
Darthpred <darthpred@gmail.com>
2012-05-02 19:13:10 +0400

    Moved datatpanels defaults from module file Renamed all panels Made options to change size of panels Made option to show/hide them Created movers for panels

--- ElvUI_DPE/modules/datatexts/add_panels.lua
+++ ElvUI_DPE/modules/datatexts/add_panels.lua
@@ -1,74 +1,72 @@
 ﻿local E, L, P, G = unpack(ElvUI); --Inport: Engine, Locales, ProfileDB, GlobalDB
+local DTP = E:NewModule('DTPanels', 'AceHook-3.0', 'AceEvent-3.0');
 local LO = E:GetModule('Layout');
-
---Defaults
-P['datatexts'] = {
-	['panels'] = {
-		['Bottom_Right'] = {
-		['left'] = 'Spell/Heal Power',
-		['middle'] = 'Haste',
-		['right'] = 'Crit Chance',
-		},
-		['Bottom_Left'] = {
-		['left'] = 'Bags',
-		['middle'] = 'Gold',
-		['right'] = 'Armor',
-		},	
-		['Top_Left'] = {
-		['left'] = 'Swatter',
-		['middle'] = 'Skada',
-		['right'] = 'MrtWoo',
-		},	
-		['Top_Right'] = {
-		['left'] = '',
-		['middle'] = '',
-		['right'] = '',
-		},	
-		['Top_Center_Left'] = {
-		['left'] = 'Altoholic',
-		['middle'] = 'TellMeWhen',
-		['right'] = 'AtlasLoot',
-		},
-		['Top_Center_Right'] = {
-		['left'] = 'Notes',
-		['middle'] = 'DBM-LDB',
-		['right'] = 'WIM',
-		},
-		['Bottom_Panel'] = 'System',
-		['Top_Center'] = 'Version',
-	},
-}
 
 --Added function to create new panels
 LO.InitializeDPE = LO.Initialize
 function LO:Initialize()
 	LO.InitializeDPE(self)
-	LO:CreateDataPanels()
+	DTP:CreateDataPanels()
+	DTP:Resize()
+	
+	E:CreateMover(DP_1, "DP_1_Mover", L["DP_1"])
+	E:CreateMover(DP_2, "DP_2_Mover", L["DP_2"])
+	E:CreateMover(DP_3, "DP_3_Mover", L["DP_3"])
+	E:CreateMover(DP_4, "DP_4_Mover", L["DP_4"])
+	E:CreateMover(DP_5, "DP_5_Mover", L["DP_5"])
+	E:CreateMover(Bottom_Panel, "Bottom_Panel_Mover", L["Bottom_Panel"])
+	E:CreateMover(DP_6, "DP_6_Mover", L["DP_6"])
 end
 
-
 -- New panels
-function LO:CreateDataPanels()
-	--bottom right
-	local bottom_bar = CreateFrame('Frame', "Bottom_Right", E.UIParent)
-	bottom_bar:SetTemplate('Default', true)
-	bottom_bar:SetFrameStrata('LOW')
-	bottom_bar:Point("BOTTOM", E.UIParent, "BOTTOM", 305, 0); 
-	bottom_bar:SetScript('OnShow', function(self) 
-		self:Size(420, 20);
-	end)
-	E:GetModule('DataTexts'):RegisterPanel(Bottom_Right, 3, 'ANCHOR_BOTTOM', 0, -4)
-	bottom_bar:Hide()
+function DTP:CreateDataPanels()
+	--the most left top panel
+	local top_left_bar = CreateFrame('Frame', "DP_1", E.UIParent)
+	top_left_bar:SetTemplate('Default', true)
+	top_left_bar:SetFrameStrata('LOW')
+	top_left_bar:Point("TOPLEFT", E.UIParent, "TOPLEFT", 0, 0); 
+	E:GetModule('DataTexts'):RegisterPanel(DP_1, 3, 'ANCHOR_BOTTOM', 0, -4)
+	top_left_bar:Hide()
+
+	--top center
+	local top_center_bar = CreateFrame('Frame', "Top_Center", E.UIParent)
+	top_center_bar:SetTemplate('Default', true)
+	top_center_bar:SetFrameStrata('LOW')
+	top_center_bar:Point("TOP", E.UIParent, "TOP", 0, 0); 
+	E:GetModule('DataTexts'):RegisterPanel(Top_Center, 1, 'ANCHOR_BOTTOM', 0, -4)
+	top_center_bar:Hide()
+	
+	--top left
+	local top_center_left_bar = CreateFrame('Frame', "DP_2", E.UIParent)
+	top_center_left_bar:SetTemplate('Default', true)
+	top_center_left_bar:SetFrameStrata('LOW')
+	top_center_left_bar:Point("TOPRIGHT", Top_Center, "TOPLEFT", -1, 0)
+	--top_center_left_bar:Point("CENTER", E.UIParent, "CENTER", 0, 0)
+	E:GetModule('DataTexts'):RegisterPanel(DP_2, 3, 'ANCHOR_BOTTOM', 0, -4)
+	top_center_left_bar:Hide()
+	
+	--top right
+	local top_center_right_bar = CreateFrame('Frame', "DP_3", E.UIParent)
+	top_center_right_bar:SetTemplate('Default', true)
+	top_center_right_bar:SetFrameStrata('LOW')
+	top_center_right_bar:Point("TOPLEFT", Top_Center, "TOPRIGHT", 1, 0); 
+	E:GetModule('DataTexts'):RegisterPanel(DP_3, 3, 'ANCHOR_BOTTOM', 0, -4)
+	top_center_right_bar:Hide()
+	
+	--the most right top panel
+	local top_right_bar = CreateFrame('Frame', "DP_4", E.UIParent)
+	top_right_bar:SetTemplate('Default', true)
+	top_right_bar:SetFrameStrata('LOW')
+	top_right_bar:Point("TOPRIGHT", E.UIParent, "TOPRIGHT", 0, 0); 
+	E:GetModule('DataTexts'):RegisterPanel(DP_4, 3, 'ANCHOR_BOTTOM', 0, -4)
+	top_right_bar:Hide()
 	
 	--bottom left
-	local top_bar = CreateFrame('Frame', 'Bottom_Left', E.UIParent)
+	local top_bar = CreateFrame('Frame', 'DP_5', E.UIParent)
 	top_bar:SetTemplate('Default', true)
 	top_bar:SetFrameStrata('LOW')
 	top_bar:Point("BOTTOM", E.UIParent, "BOTTOM", -305, 0); 
-	top_bar:SetScript('OnShow', function(self) 
-		self:Size(420, 20);
-	end)
-	E:GetModule('DataTexts'):RegisterPanel(Bottom_Left, 3, 'ANCHOR_BOTTOM', 0, -4)
+	E:GetModule('DataTexts'):RegisterPanel(DP_5, 3, 'ANCHOR_BOTTOM', 0, -4)
 	top_bar:Hide()
 	
 	--bottom center
@@ -76,83 +74,85 @@ function LO:CreateDataPanels()
 	map:SetTemplate('Default', true)
 	map:SetFrameStrata('LOW')
 	map:Point("BOTTOM", E.UIParent, "BOTTOM", 0, 0); 
-	map:SetScript('OnShow', function(self) 
-		self:Size(188, 20)
-	end)
 	E:GetModule('DataTexts'):RegisterPanel(Bottom_Panel, 1, 'ANCHOR_BOTTOM', 0, -4)
 	map:Hide()
 	
-	--the most left top panel
-	local top_left_bar = CreateFrame('Frame', "Top_Left", E.UIParent)
-	top_left_bar:SetTemplate('Default', true)
-	top_left_bar:SetFrameStrata('LOW')
-	top_left_bar:SetScript('OnShow', function(self) 
-		self:Point("TOPLEFT", E.UIParent, "TOPLEFT", 0, 0); 
-		self:Size(E.UIParent:GetWidth()/5, 20);
-	end)
-	E:GetModule('DataTexts'):RegisterPanel(Top_Left, 3, 'ANCHOR_BOTTOM', 0, -4)
-	top_left_bar:Hide()
-	
-	--the most right top panel
-	local top_right_bar = CreateFrame('Frame', "Top_Right", E.UIParent)
-	top_right_bar:SetTemplate('Default', true)
-	top_right_bar:SetFrameStrata('LOW')
-	top_right_bar:SetScript('OnShow', function(self) 
-		self:Point("TOPRIGHT", E.UIParent, "TOPRIGHT", 0, 0); 
-		self:Size(E.UIParent:GetWidth()/5, 20);
-	end)
-	E:GetModule('DataTexts'):RegisterPanel(Top_Right, 3, 'ANCHOR_BOTTOM', 0, -4)
-	top_right_bar:Hide()
-	
-	--top right
-	local top_center_right_bar = CreateFrame('Frame', "Top_Center_Right", E.UIParent)
-	top_center_right_bar:SetTemplate('Default', true)
-	top_center_right_bar:SetFrameStrata('LOW')
-	top_center_right_bar:SetScript('OnShow', function(self) 
-		self:Point("TOPRIGHT", Top_Right, "TOPLEFT", -1, 0); 
-		self:Size(E.UIParent:GetWidth()/5, 20);
-	end)
-	E:GetModule('DataTexts'):RegisterPanel(Top_Center_Right, 3, 'ANCHOR_BOTTOM', 0, -4)
-	top_center_right_bar:Hide()
-	
-	--top left
-	local top_center_left_bar = CreateFrame('Frame', "Top_Center_Left", E.UIParent)
-	top_center_left_bar:SetTemplate('Default', true)
-	top_center_left_bar:SetFrameStrata('LOW')
-	top_center_left_bar:SetScript('OnShow', function(self) 
-		self:Point("TOPLEFT", Top_Left, "TOPRIGHT", 1, 0); 
-		self:Size(E.UIParent:GetWidth()/5, 20);
-	end)
-	E:GetModule('DataTexts'):RegisterPanel(Top_Center_Left, 3, 'ANCHOR_BOTTOM', 0, -4)
-	top_center_left_bar:Hide()
-	
-	--top center
-	local top_center_bar = CreateFrame('Frame', "Top_Center", E.UIParent)
-	top_center_bar:SetTemplate('Default', true)
-	top_center_bar:SetFrameStrata('LOW')
-	top_center_bar:SetScript('OnShow', function(self) 
-		self:Point("TOP", E.UIParent, "TOP", 0, 0); 
-		self:Size((E.UIParent:GetWidth()/5) - 4, 20);
-	end)
-	E:GetModule('DataTexts'):RegisterPanel(Top_Center, 1, 'ANCHOR_BOTTOM', 0, -4)
-	top_center_bar:Hide()
+	--bottom right
+	local bottom_bar = CreateFrame('Frame', "DP_6", E.UIParent)
+	bottom_bar:SetTemplate('Default', true)
+	bottom_bar:SetFrameStrata('LOW')
+	bottom_bar:Point("BOTTOM", E.UIParent, "BOTTOM", 305, 0); 
+	E:GetModule('DataTexts'):RegisterPanel(DP_6, 3, 'ANCHOR_BOTTOM', 0, -4)
+	bottom_bar:Hide()
+end
+
+function DTP:Resize()
+	DP_5:Size(E.db.dpe.datatext.dp5.width, 20)
+	DP_6:Size(E.db.dpe.datatext.dp6.width, 20)
+	Bottom_Panel:Size(E.db.dpe.datatext.bottom.width, 20)
+	DP_1:Size(E.db.dpe.datatext.dp1.width, 20)
+	DP_4:Size(E.db.dpe.datatext.dp4.width, 20)
+	DP_3:Size(E.db.dpe.datatext.dp3.width, 20)
+	DP_2:Size(E.db.dpe.datatext.dp2.width, 20)
+	Top_Center:Size(E.db.dpe.datatext.top.width, 20)
 end
 
 --Showing panels
-function ExtraDataBarSetup()
-Bottom_Left:Show()
-Bottom_Right:Show()
-Bottom_Panel:Show()
-Top_Left:Show()
-Top_Right:Show()
-Top_Center_Right:Show()
-Top_Center_Left:Show()
+function DTP:ExtraDataBarSetup()
+	if E.db.dpe.datatext.dp1.enabled then
+		DP_1:Show()
+	else
+		DP_1:Hide()
+	end
+	
+	if E.db.dpe.datatext.dp2.enabled then
+		DP_2:Show()
+	else
+		DP_2:Hide()
+	end
+	
+	if E.db.dpe.datatext.dp3.enabled then
+		DP_3:Show()
+	else
+		DP_3:Hide()
+	end
+	
+	if E.db.dpe.datatext.dp4.enabled then
+		DP_4:Show()
+	else
+		DP_4:Hide()
+	end
+	
+	if E.db.dpe.datatext.dp5.enabled then
+		DP_5:Show()
+	else
+		DP_5:Hide()
+	end
+	
+	if E.db.dpe.datatext.dp6.enabled then
+		DP_6:Show()
+	else
+		DP_6:Hide()
+	end
+	
+	if E.db.dpe.datatext.bottom.enabled then
+		Bottom_Panel:Show()
+	else
+		Bottom_Panel:Hide()
+	end
 Top_Center:Show()
 end
 
+function DTP:Update()
+	DTP:ExtraDataBarSetup()
+	DTP:Resize()
+end
+
 --Renew panels after loading screens
-function LO:PLAYER_ENTERING_WORLD(...)
-ExtraDataBarSetup()
+function DTP:PLAYER_ENTERING_WORLD(...)
+DTP:ExtraDataBarSetup()
 self:UnregisterEvent("PLAYER_ENTERING_WORLD");
 end
-LO:RegisterEvent('PLAYER_ENTERING_WORLD')
+DTP:RegisterEvent('PLAYER_ENTERING_WORLD')
+
+E:RegisterModule(DTP:GetName())
