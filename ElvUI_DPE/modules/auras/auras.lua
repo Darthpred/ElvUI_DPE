8fffed5de42843ad169e145a4b0e5810ed7c7541
Darthpred <darthpred@gmail.com>
2012-05-04 10:41:09 +0400

    Added buff's caster name in tooltip. Credit - Repooc

--- ElvUI_DPE/modules/auras/auras.lua
+++ ElvUI_DPE/modules/auras/auras.lua
@@ -33,3 +33,43 @@ function A:StyleBuffs(buttonName, index, debuff)
 	end
 	if border then border:Hide() end
 end
+
+function addAuraSource(self, func, unit, index, filter)
+	local srcUnit = select(8, func(unit, index, filter))
+	if srcUnit then
+		self:AddLine(" ")
+		
+		local src = GetUnitName(srcUnit, true)
+		if srcUnit == "pet" or srcUnit == "vehicle" then
+			src = format("%s (%s)", src, GetUnitName("player", true))
+		else
+			local partypet = srcUnit:match("^partypet(%d+)$")
+			local raidpet = srcUnit:match("^raidpet(%d+)$")
+			if partypet then
+				src = format("%s (%s)", src, GetUnitName("party"..partypet, true))
+			elseif raidpet then
+				src = format("%s (%s)", src, GetUnitName("raid"..raidpet, true))
+			end
+		end
+		
+		self:AddLine(src)
+		self:Show()
+	end
+end
+
+A.InitializeDPE = A.Initialize
+function A:Initialize()
+    A.InitializeDPE(self)
+	
+	local funcs = {
+		SetUnitAura = UnitAura,
+		SetUnitBuff = UnitBuff,
+		SetUnitDebuff = UnitDebuff,
+	}
+
+	for k, v in pairs(funcs) do
+		hooksecurefunc(GameTooltip, k, function(self, unit, index, filter)
+		addAuraSource(self, v, unit, index, filter)
+		end)
+	end
+end
