eced199214a63ddc3c01163657f9db403e8e83c6
Darthpred <darthpred@gmail.com>
2012-05-05 13:34:31 +0400

    Reworked English locale file to be smaller. No need in lots of duplicating "translations"

--- ElvUI_DPE/locales/english.lua
+++ ElvUI_DPE/locales/english.lua
@@ -10,7 +10,7 @@ end
 if not L then return; end
 
 --Main config
-L["Additional options"] = "Additional options"
+L["Additional options"] = true
 L["Darth Predator's edit of ElvUI"] = "Darth Predator's edit of ElvUI"
 L['DPE_DESC'] = [=[This plugin changes ElvUI according to my personal taste. For maximum and comfortable usage 1920x1080 resolution is recommended.
 
@@ -18,72 +18,78 @@ Plugin doesn't change any files in ElvUI folder so disabling or deleting it woul
 ]=]
 
 --LFR Lockdown info
-L["LFR Dragon Soul"] = "LFR Dragon Soul"
-L["LFR Lockdown"] = "LFR Lockdown"
-L["Show/Hide LFR lockdown info in time datatext's tooltip."] = "Show/Hide LFR lockdown info in time datatext's tooltip."
+L["LFR Dragon Soul"] = true
+L["LFR Lockdown"] = true
+L["Show/Hide LFR lockdown info in time datatext's tooltip."] = true
 
 --PvP auto release
-L["PvP Autorelease"] = "PvP Autorelease"
-L['Automatically release body when killed inside a battleground.'] = "Automatically release body when killed inside a battleground."
+L["PvP Autorelease"] = true
+L['Automatically release body when killed inside a battleground.'] = true
 
 --Auras
-L['Aura Size'] = "Aura Size"
-L['Sets size of auras.\n|cffFF0000This is global setting and will affect all your profiles.|r'] = "Sets size of auras.\n|cffFF0000This is global setting and will affect all your profiles.|r"
+L['Aura Size'] = true
+L['Sets size of auras.\n|cffFF0000This is global setting and will affect all your profiles.|r'] = true
 
 --Skada
-L["Skada Backdrop"]= "Skada Backdrop"
-L['Show/hide Skada backdrop.'] = "Show/hide Skada backdrop."
+L["Skada Backdrop"]= true
+L['Show/hide Skada backdrop.'] = true
 
 --Player Frame Indicators
-L["Player Frame Indicators"] = "Player Frame Indicators"
-L["PvP text on mouse over"] = "PvP text on mouse over"
-L['Show PvP text on mouse over player frame.'] = "Show PvP text on mouse over player frame. Will be shown constantly if toggled of."
-L["PvP Position"] = "PvP Position"
-L['Set the point to show pvp text'] = "Set the point to show pvp text"
-L["Combat Position"] = "Combat Position"
-L['Set the point to show combat icon'] = "Set the point to show combat icon"
+L["Player Frame Indicators"] = true
+L["PvP text on mouse over"] = true
+L['Show PvP text on mouse over player frame.'] = true
+L["PvP Position"] = true
+L['Set the point to show pvp text'] = true
+L["Combat Position"] = true
+L['Set the point to show combat icon'] = true
 
 --Raid Marks
-L["Marks"] = "Marks"
-L["Show/Hide raid marks."] = "Show/Hide raid marks."
-L['Sets size of buttons'] = "Sets size of buttons"
-L["Direction"] = "Direction"
-L['Change the direction of buttons growth from "skull" mark'] = 'Change the direction of buttons growth from "skull" mark'
+L["Marks"] = true
+L["Show/Hide raid marks."] = true
+L['Sets size of buttons'] = true
+L["Direction"] = true
+L['Change the direction of buttons growth from "skull" mark'] = true
+
+--Quest Tracker
+L["Quest Tracker"] = true
+L['Quest Tracker Backdrop'] = true
+L['Attaches a backdrop frame behind the quest tracker.'] = true
+L["Sets size of quest tracker's font"] = true
 
 --Exp/Rep Text--
 ----------------
-L["Xp-Rep Text"] = "Exp/Rep Text"
-L["XP-Rep Text mod by Benik"] = "XP-Rep Text mod by Benik."
-L['Show/Hide XP-Rep Info.'] = "Show/Hide XP-Rep Info"
-L['Detailed'] = "Detailed"
-L['More XP-Rep Info. Shown only when bars are on top.'] = "More XP-Rep Info. Shown only when bars are on top."
-L["Detailed options"] = "Detailed options"
-L['Reaction Name'] = "Reaction Name"
-L['Show/Hide Reaction status on bar.'] = "Show/Hide Reaction status on bar."
-L['Rested Value'] = "Rested Value"
-L['Show/Hide Rested value.'] = "Show/Hide Rested value."
+L["Xp-Rep Text"] = true
+L["XP-Rep Text mod by Benik"] = true
+L['Show/Hide XP-Rep Info.'] = true
+L['Detailed'] = true
+L['More XP-Rep Info. Shown only when bars are on top.'] = true
+L["Detailed options"] = true
+L['Reaction Name'] = true
+L['Show/Hide Reaction status on bar.'] = true
+L['Rested Value'] = true
+L['Show/Hide Rested value.'] = true
 
 --Backgrounds--
 ---------------
-L["Backgrounds"] = "Backgrounds"
-L["Additional Background Panels"] = "Additional Background Panels"
+L["Backgrounds"] = true
+L["Additional Background Panels"] = true
 L["BG_DESC"] = "Module to create additional frames which can be used as backgrounds for something."
-L["Bottom BG"] = "Bottom BG"
-L["Left BG"] = "Left BG"
-L["Right BG"] = "Right BG"
-L["Actionbar BG"] = "Actionbar BG"
-L['Show/Hide this frame.'] = "Show/Hide this frame."
-L['Sets width of the frame'] = "Sets width of the frame"
-L['Sets height of the frame'] = "Sets height of the frame"
-L['Sets X offset of the frame'] = "Sets X offset of the frame"
-L['Sets Y offset of the frame'] = "Sets Y offset of the frame"
-L["Texture"] = "Texture"
-L["Set texture to use in this frame. Requirements are the same as for the chat textures."] = "Set texture to use in this frame. Requirements are the same as for the chat textures."
+L["Bottom BG"] = true
+L["Left BG"] = true
+L["Right BG"] = true
+L["Actionbar BG"] = true
+L['Show/Hide this frame.'] = true
+L['Sets width of the frame'] = true
+L['Sets height of the frame'] = true
+L['Sets X offset of the frame'] = true
+L['Sets Y offset of the frame'] = true
+L["Texture"] = true
+L["Set texture to use in this frame. Requirements are the same as for the chat textures."] = true
 
 --Datatexts--
 -------------
-L["Datatext panels"] = "Datatext panels"
-L["Additional Datatext Panels"] = "Additional Datatext Panels"
+L["Datatext panels"] = true
+L["Additional Datatext Panels"] = true
 L["DP_DESC"] = [=[Additional datatext panels.
 8 panels with 20 datatext points total.
 You can't disable Top Panel and chat panels.]=]
@@ -100,27 +106,48 @@ L['Sets size of this panel'] = "Sets size of this panel"
 
 --Raid Utility--
 ----------------
-L["Raid Utility"] = "Raid Utility"
-L["Raid Utility coordinates"] = "Raid Utility coordinates"
+L["Raid Utility"] = true
+L["Raid Utility coordinates"] = true
 L["RU_DESC"] = [=[This config group allows you to freely move your Raid Utility button starting position.
 Unfortunately movement of this buttom with mouse will produse errors after clicking on the moved button.
 Those errors are not critical but may cause perfomance issues in case of long and active RU usage.]=]
-L['X Position'] = "X Position"
-L['Sets X position of Raid Utility button.'] = "Sets X position of Raid Utility button."
-L['Y Position'] = "Y Position"
-L['Sets Y position of Raid Utility button.'] = "Sets Y position of Raid Utility button."
+L['X Position'] = true
+L['Sets X position of Raid Utility button.'] = true
+L['Y Position'] = true
+L['Sets Y position of Raid Utility button.'] = true
+
+--UI Buttons--
+--------------
+L["UI Buttons"] = true
+L["Additional menu with useful buttons"] = true
+L["Show/Hide UI buttons."] = true
+L["Mouse over"] = true
+L["Show on mouse over."] = true
+L["Buttons position"] = true
+L["Layout for UI buttons."] = true
+
+L["ElvUI Config"] = true
+L["Click to toggle config window"] = true
+L["Reload UI"] = true
+L["Click to reload your interface"] = true
+L["Move UI"] = true
+L["Click to unlock moving ElvUI elements"] = true
+L["Boss Mod"] = true
+L["Click to toogle the Configuration/Option Window from the Bossmod (DXE, DBM or Bigwigs) you have enabled."] = true
+L["AddOns Manager"] = true
+L["Click to toogle the AddOn Managerframe (stAddOnManager or ACP) you have enabled."] = true
 
 --Balance Power Frame --
 ------------------------
-L["Druid"] = "Druid"
-L["Druid spesific options"] = "Druid spesific options"
-L["Balance Power Frame"] = "Balance Power Frame"
-L["Show/hide the frame with exact number of your Solar/Lunar energy."] = "Show/hide the frame with exact number of your Solar/Lunar energy."
+L["Druid"] = true
+L["Druid spesific options"] = true
+L["Balance Power Frame"] = true
+L["Show/hide the frame with exact number of your Solar/Lunar energy."] = true
 
 --Credits--
 -----------
 L['ELVUI_DPE_CREDITS'] = "I would like to point out the following people for helping me creating this addon with testing, coding and other stuff."
-L['Submodules and coding:'] = "Submodules and coding:"
+L['Submodules and coding:'] = true
 L['ELVUI_DPE_CODERS'] = [=[Benik - core of exp/rep bars' text
 Catok - Test UI feature
 Repooc - core of auto release feature
@@ -129,7 +156,7 @@ Pvtschlag - Necrotic Strike oUF plugin
 Blazeflack - helping hooking, modules and profiles
 Camealion - teaching me the art of skining
 ]=]
-L['Other support:'] = "Other support:"
+L['Other support:'] = true
 L['ELVUI_DPE_MISC'] = [=[BuG - brining fun to the chat while i was writing this
 TheSamaKutra - some good ideas
 Allaidia - moral support O_o
